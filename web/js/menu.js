df6e94214c96e2ed776c2d62f18ee7f32b204dbb
DOLLE <jeremy.dolle34@gmail.com>
2017-02-15 11:37:06 +0100

    fix the title for pages, complete the home page to make it responsive and have an indication what will do each menu, add the js to open and close the side menu

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,32 +1,3 @@
 /**
- * Created by jeremy on 08/02/2017.
+ * Created by jeremy on 15/02/2017.
  */
-
-$(document).ready(function(){
-    $('#circleMenu').click(function () {
-        if( $(this).hasClass('active'))
-        {
-            $(this).removeClass('active');
-            // $(this).text('<i class="fa fa-bars"></i>');
-            $('#overlay').removeClass('active');
-
-            $('.menu-list .menu').each(function(){
-                $(this).removeClass('active');
-            });
-        }
-        else
-        {
-            $(this).addClass('active');
-            // $(this).text('x');
-            $('#overlay').addClass('active');
-
-            $('.menu-list .menu').each(function(){
-                $(this).addClass('active');
-            });
-        }
-    });
-
-    $('#menu-toggle').click(function(){
-        $(this).toggleClass('open');
-    })
-});
